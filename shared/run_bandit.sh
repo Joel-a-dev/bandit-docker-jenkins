c65c076c30a6371c8262e4fa60476b61fb5ed0df
Joel Almeida <joel.almeida@ibm.com>
2019-01-18 03:11:27 +0000

    f

--- shared/run_bandit.sh
+++ shared/run_bandit.sh
@@ -5,7 +5,7 @@
 REPORT_HTML='banditReport.html'
 REPORT_TXT='banditReport.txt'
 
-bandit -r -f txt -o shared/${REPORT_TXT} . &>/dev/null
-bandit -r -f html -o shared/${REPORT_HTML} . &>/dev/null
+bandit -r -f txt -o shared/${REPORT_TXT} . 
+bandit -r -f html -o shared/${REPORT_HTML} .
 
 echo $(cat shared/"${REPORT_TXT}")
