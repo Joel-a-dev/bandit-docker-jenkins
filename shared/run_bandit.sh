a63250b50361797e8c31a03b389f3a06eb487fa8
Joel Almeida <joel.almeida@ibm.com>
2019-01-18 02:07:25 +0000

    silent bandit runs and txt echo

--- shared/run_bandit.sh
+++ shared/run_bandit.sh
@@ -5,5 +5,7 @@
 REPORT_HTML='banditReport.html'
 REPORT_TXT='banditReport.txt'
 
-bandit -r -f txt -o shared/${REPORT_TXT} .
-bandit -r -f html -o shared/${REPORT_HTML} .
+bandit -r -q -f txt -o shared/${REPORT_TXT} .
+bandit -r -q -f html -o shared/${REPORT_HTML} .
+
+echo $(cat shared/"${REPORT_TXT}")
