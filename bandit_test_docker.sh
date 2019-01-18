ec0a2001209114fef506bd5bb462cf0305f5132f
Joel Almeida <joel.almeida@ibm.com>
2019-01-18 04:11:34 +0000

    assa

--- bandit_test_docker.sh
+++ bandit_test_docker.sh
@@ -7,4 +7,4 @@ chmod +x ./shared/run_bandit.sh
 set -e
 
 docker build -t ${BANDIT_IMAGE}:${BANDIT_TAG} -f ${DOCKERFILE} .
-docker run --name ${CONTAINER} -v ${PWD}/shared/:/bandit/app ${BANDIT_IMAGE}:${BANDIT_TAG} 
+docker run --name ${CONTAINER} -v ${PWD}/shared/:/bandit/shared ${BANDIT_IMAGE}:${BANDIT_TAG} 
