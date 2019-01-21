4d32950ec61abf558a9326542ecbd101366995cd
Joel Almeida <joel.almeida@ibm.com>
2019-01-21 09:31:32 +0000

    a

--- .bandit/run_bandit.sh
+++ .bandit/run_bandit.sh
@@ -5,6 +5,8 @@ BANDIT_CONFIG='/app_tmp/.bandir/bandit.config.yaml'
 REPORT_HTML='/app_tmp/.bandit/reports/banditReport.html'
 REPORT_TXT='/app_tmp/.bandit/reports/banditReport.txt'
 
+mkdir '/app_tmp/.bandit/reports'
+
 bandit -r -c ${BANDIT_CONFIG}  -f txt -o ${REPORT_TXT} ../.
 cat ${REPORT_TXT}
 bandit -r -c ${BANDIT_CONFIG}  -f html -o ${REPORT_HTML} ../.
