0e2063890bc8bc6f8569666c5aac4a8aaa5114bf
Joel Almeida <joel.almeida@ibm.com>
2019-01-21 18:01:12 +0000

    a

--- bandit/bandit_test_docker.sh
+++ bandit/bandit_test_docker.sh
@@ -2,7 +2,7 @@
 cd ..
 DOCKERFILE='bandit/bandit.Dockerfile'
 PWD=$(pwd)
-mkdir reports
+ls -Rla
 set -e
 # TODO : decide if the image is to be built every run or pulled from docker repository or existent in local docker repository
 docker build -t ${BANDIT_IMAGE}:${BANDIT_TAG} -f ${DOCKERFILE} .
