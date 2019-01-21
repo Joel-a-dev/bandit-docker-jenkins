21966f460074fbff7e54db7464de018549a9bc57
Joel Almeida <joel.almeida@ibm.com>
2019-01-21 11:58:37 +0000

    a

--- .bandit/bandit.Dockerfile
+++ .bandit/bandit.Dockerfile
@@ -2,8 +2,8 @@ FROM python:3.6-slim
 
 COPY . /app_tmp
 
-RUN pip install bandit      && \
-    mkdir -p /app_tmp       && \
+RUN pip install bandit                    && \
+    mkdir /app_tmp/.bandit/reports      && \
     chown -R $USER: /app_tmp
 RUN chmod a+x /app_tmp/.bandit/run_bandit.sh
 WORKDIR /app_tmp
--- .bandit/run_bandit.sh
+++ .bandit/run_bandit.sh
@@ -5,8 +5,6 @@ BANDIT_CONFIG='/app_tmp/.bandit/bandit.config.yaml'
 REPORT_HTML='/app_tmp/.bandit/reports/banditReport.html'
 REPORT_TXT='/app_tmp/.bandit/reports/banditReport.txt'
 
-mkdir '/app_tmp/.bandit/reports'
-
 bandit -r -f txt -o ${REPORT_TXT} /app_tmp
 cat ${REPORT_TXT}
 bandit -r -f html -o ${REPORT_HTML} /app_tmp
