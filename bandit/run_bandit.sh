cd06885ee0e996050e415c5bd79a0f3940ab39f4
Joel Almeida <joel.almeida@ibm.com>
2019-01-25 17:28:57 +0000

    asdasdsadasdasd

--- bandit/run_bandit.sh
+++ bandit/run_bandit.sh
@@ -3,11 +3,8 @@
 BANDIT_CONFIG='/app_src/bandit/bandit.config.yaml'
 REPORT_HTML='/app_src/reports/banditReport.html'
 REPORT_TXT='/app_src/reports/banditReport.txt'
-
 pip3 install bandit
-
 mkdir reports
-chmod -R 777 /app_src
 
 bandit -r -f txt -o ${REPORT_TXT} /app_src
 cat ${REPORT_TXT}
